7fe3d3d897fd7aca524f5b35e62cd5e3f659089b
jenil <jo287@njit.edu>
2021-07-05 23:49:17 -0400

    Commit 8 - calculator - Test addition method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -16,45 +16,45 @@ class MyTestCase(unittest.TestCase):
 
     def test_add_method_calculator(self):
         test_data = CsvReader('/src/add.csv').data
-        pprint(test_data)
+        #pprint(test_data)
         for row in test_data:
             self.assertEqual(self.calculator.add(row['\ufeffValue 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_subtract_method_calculator(self):
-        test_data = CsvReader('/src/subtraction.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['\ufeffValue 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
+    # def test_subtract_method_calculator(self):
+    #     test_data = CsvReader('/src/subtraction.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.subtract(row['\ufeffValue 1'], row['Value 2']), int(row['Result']))
+    #         self.assertEqual(self.calculator.result, int(row['Result']))
 
-    def test_multiplication_method_calculator(self):
-        test_data = CsvReader('/src/multiplication.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.multiplication(row['\ufeffValue 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
-    def test_division_method_calculator(self):
-        test_data = CsvReader('/src/division.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.division(row['\ufeffValue 1'], row['Value 2']), float(row['Result']))
-            self.assertEqual(self.calculator.result, float(row['Result']))
-
-    def test_sqrnum_method_calculator(self):
-        test_data = CsvReader('/src/sqr.csv').data
-        # pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.sqrnum(row['\ufeffValue 1']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
-    def test_sqrroot_method_calculator(self):
-        test_data = CsvReader('/src/sqrroot.csv').data
-        pprint(test_data)
-        for row in test_data:
-            self.assertEqual(self.calculator.sqrroot(row['\ufeffValue 1']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
+    # def test_multiplication_method_calculator(self):
+    #     test_data = CsvReader('/src/multiplication.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.multiplication(row['\ufeffValue 1'], row['Value 2']), int(row['Result']))
+    #         self.assertEqual(self.calculator.result, int(row['Result']))
+    #
+    # def test_division_method_calculator(self):
+    #     test_data = CsvReader('/src/division.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.division(row['\ufeffValue 1'], row['Value 2']), float(row['Result']))
+    #         self.assertEqual(self.calculator.result, float(row['Result']))
+    #
+    # def test_sqrnum_method_calculator(self):
+    #     test_data = CsvReader('/src/sqr.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.sqrnum(row['\ufeffValue 1']), int(row['Result']))
+    #         self.assertEqual(self.calculator.result, int(row['Result']))
+    #
+    # def test_sqrroot_method_calculator(self):
+    #     test_data = CsvReader('/src/sqrroot.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.sqrroot(row['\ufeffValue 1']), int(row['Result']))
+    #         self.assertEqual(self.calculator.result, int(row['Result']))
 
 if __name__ == '__main__':
     unittest.main()
